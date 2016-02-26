2b45cbaab691ebf8f557612ba22913d01b4d9923
Lauri Võsandi <lvosandi@itcollege.ee>
2016-02-26 13:14:01 +0200

    Add remove item from cart button

--- cart.php
+++ cart.php
@@ -5,11 +5,16 @@ $conn = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 $conn or die("Database connection failed:" . $conn->error);
 $conn->query("set names utf8"); // Support umlaut characters
 
-$product_id = intval($_POST["id"]);
-if (array_key_exists($product_id, $_SESSION["cart"])) {
-    $_SESSION["cart"][$product_id] += 1;
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    // We are adding something to the cart
+    $product_id = intval($_POST["id"]);
+    if (array_key_exists($product_id, $_SESSION["cart"])) {
+        $_SESSION["cart"][$product_id] += 1;
+    } else {
+        $_SESSION["cart"][$product_id] = 1;
+    }
 } else {
-    $_SESSION["cart"][$product_id] = 1;
+    // We are just checking out what we have in the cart
 }
 
 ?>
@@ -27,13 +32,17 @@ $results or die("Database query failed:" . $conn->error);
 while ($row = $results->fetch_assoc()) {
   $product_id = $row['id'];
   if (array_key_exists($product_id, $_SESSION["cart"])) {
-    $count = $_SESSION["cart"][];
+    $count = $_SESSION["cart"][$product_id];
     ?>
       <li>
         <?=$count;?> items of
         <a href="description.php?id=<?=$product_id;?>">
           <?=$row['name'];?></a>
           <?=$row['price'];?>EUR totals in <?= $row['price'] * $count; ?> EUR
+        <form method="delete">
+          <input type="hidden" name="id" value="<?=$product_id;?>"/>
+          <input type="submit" value="Remove"/>
+        </form>
       </li>
     <?php
   }
