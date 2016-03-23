0ea76ea47311ae79ea02c48378e52b92a9c037f0
Lauri Võsandi <lauri.vosandi@gmail.com>
2016-03-23 14:27:38 +0200

    Add login buttons to index.php

--- index.php
+++ index.php
@@ -15,12 +15,44 @@ if (!array_key_exists("timestamp", $_SESSION)) {
 
 <h1>Honest Lauri's webshop</h1>
 
-<p><a href="registration.php">Sign up!</a>. <a href="cart.php">Go to shopping cart</a>.</p>
+<p>
+
+<!-- We could actually move login and registration buttons to
+     separate nav.php file and include it here -->
+
+<?php
+
+if (array_key_exists("user", $_SESSION)) {
+    // In case we put user id in the $_SESSION["user"] we need
+    // to perform another SQL query to get the full name of the user:
+    $results = $conn->query(
+        "SELECT * FROM lauri_users
+        WHERE id = " . $_SESSION["user"]);
+    $row = $results->fetch_assoc();
+    echo "Hello " . $row["salutation"] . " ";
+    echo $row["first_name"] . " ";
+    echo $row["last_name"];
+
+    // Oh how I'd like to do just:
+    // print "Hello %(salutation)s %(first_name)s %(last_name)s" % $row
+
+    ?> <a href="logout.php">Log out</a><?php
+} else {
+   // Otherwise offer login fields and button
+   ?>
+   <form action="login.php" method="post">
+     <input type="text" name="user"/>
+     <input type="password" name="password"/>
+     <input type="submit" value="Log in!"/>
+   </form><?php
+} ?>
+
+<a href="registration.php">Sign up!</a>. <a href="cart.php">Go to shopping cart</a>.</p>
 
 <p>NSA is monitoring you since <?=$_SESSION["timestamp"];?></p>
 <p>If you want any of these just call me ;)</p>
 <ul>
-<?php 
+<?php
 $results = $conn->query(
 "SELECT id,name,price FROM lauri_products;");
 
